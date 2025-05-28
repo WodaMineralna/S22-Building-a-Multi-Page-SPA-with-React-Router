ac507d63342cb5d6fbba029c4fab1521ff3ab258
WodaMineralna <raknieborak65@gmail.com>
2025-05-28 14:44:34 +0200

    Highlight active navigation link using <NavLink> in MainNavigation.js
    - Switched from <Link> to <NavLink>

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-import classes  from "./MainNavigation.module.css"
+import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation() {
   return (
@@ -9,10 +9,24 @@ export default function MainNavigation() {
         <nav>
           <ul className={classes.list}>
             <li>
-              <Link to="/">Home</Link>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Home
+              </NavLink>
             </li>
             <li>
-              <Link to="/products">Products</Link>
+              <NavLink
+                to="/products"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Products
+              </NavLink>
             </li>
           </ul>
         </nav>
