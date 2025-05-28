cee094d351838c2ec91917cdc9645e441a6ca910
WodaMineralna <raknieborak65@gmail.com>
2025-05-28 13:38:06 +0200

    Added basic styling for Components
    - Moved the MainNavigation.js Component into the 'components' folder

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,12 +1,15 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
-import MainNavigation from "./MainNavigation"
+import MainNavigation from "../components/MainNavigation";
+import classes from "./Root.module.css";
 
 export default function RootLayout() {
   return (
     <>
       <MainNavigation />
-      <Outlet />
+      <main className={classes.content}>
+        <Outlet />
+      </main>
     </>
-  )
+  );
 }
