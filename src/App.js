a36716978ac57fc60d1807c932e58ec60d956af4
WodaMineralna <raknieborak65@gmail.com>
2025-05-28 13:48:54 +0200

    Created an ErrorPage Component, which will display if an error occurs [...]   -> [...] eg. when user tries to acces a page that doesnt exist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
-import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
